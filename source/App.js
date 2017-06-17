615ae8e05c2a91ce32b616fb388a2507bb389965
Ben Larking <benjamin.o.larking@accenture.com>
2017-06-17 14:16:28 +0100

    Creating the foundations for my Kanban Application

--- source/App.js
+++ source/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, {
+    Component
+} from 'react';
 import ReactDOM from 'react-dom';
+import KanbanBoard from './KanbanBoard';
 
-class Hello extends React.Component {
-  render() {
-    return (
-      <h1>Hello World</h1>
-    )
-  }
-}
+let cardsList = [
+    {
+        id: 1,
+        title: "Build Kanban board app",
+        description: "My React KanbanBoard",
+        status: "in-progress",
+        tasks: []
+    },
+    {
+        id: 2,
+        title: "Write some code",
+        description: "Code the Kanban application",
+        status: "todo",
+        tasks: [
+            {
+                id: 1,
+                name: "ContactList Example",
+                done: true
+            },
+            {
+                id: 2,
+                name: "Kanban Example",
+                done: false
+            },
+            {
+                id: 3,
+                name: "My own experiments",
+                done: false
+            }
+        ]
+    },
+];
 
-
-ReactDOM.render(<Hello />,document.getElementById("root"));
+ReactDOM.render( <KanbanBoard cards={cardsList} / > , document.getElementById("root"));
